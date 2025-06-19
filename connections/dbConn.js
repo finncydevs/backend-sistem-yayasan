a3a569309673bc0e17437aa425a559dbfc8f12cb
finn <finnnxptrix@gmail.com>
2025-06-19 08:42:07 +0700

    commit

--- connections/dbConn.js
+++ connections/dbConn.js
@@ -2,11 +2,12 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_PASSWORD,
 });
+console.log(process.env.DB_HOST)
 
 db.connect((err) => {
   if (err) throw err;
--- server.js
+++ server.js
@@ -4,24 +4,31 @@ const cors = require("cors");
 const { errorHandler } = require("./middleware/errorHandler");
 const db = require("./connections/dbConn");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
-  
+
 app.use(cors());
 app.use(express.json()); // ⚠️ MUST come before routes
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api/admin", require("./routes/adminRoutes"));
-app.use("/api/tapel", require("./routes/tapelRoutes"));
-app.use("/api/lembaga", require("./routes/lembagaRoutes"));
-app.use("/api/pegawai", require("./routes/pegawaiRoutes"));
-app.use("/api/spk", require("./routes/satuanPdkRoutes"));
-app.use("/api/nomorSurat", require("./routes/nomorSuratRoutes"));
-app.use("/api/penugasan", require("./routes/penugasanRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/tapel", require("./routes/tapelRoutes"));
+app.use("/api/v1/lembaga", require("./routes/lembagaRoutes"));
+app.use("/api/v1/pegawai", require("./routes/pegawaiRoutes"));
+app.use("/api/v1/spk", require("./routes/satuanPdkRoutes"));
+app.use("/api/v1/nomorSurat", require("./routes/nomorSuratRoutes"));
+app.use("/api/v1/penugasan", require("./routes/penugasanRoutes"));
 
 app.use(errorHandler);
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
+});
 
 const port = process.env.PORT || 3000;
 
